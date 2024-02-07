720484848b3b8b48c9e4f1e0dec385ed4a5fa258
Matacos3 <acosta.garcia.mateo@gmail.com>
2024-02-07 14:29:43 +0100

    premier commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+const fetch = require("node-fetch")
+
+const key = process.env.OWM_API_KEY
+
+router.get("/movies",(req, res) =>{
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}`)
+    .then(response=>response.json())
+    .then(data =>{
+      console.log(data)
+      res.json({movies : data.results})
+    })
+
+})  
+
+
+module.exports = router;
